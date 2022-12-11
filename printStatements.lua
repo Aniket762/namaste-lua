40c590e97776a14438db678a71f8605ee77415dd
Aniket762 <aniketindian8@gmail.com>
2022-12-11 21:48:21 +0530

    docs: print statements

---
+++ printStatements.lua
@@ -0,0 +1,8 @@
+-- Print Statement
+print('Hello World')
+
+-- Comma gives a tab in between the statements that get printed
+print("Part 1","Part 2")
+
+-- When we write a number in print, it converts into a string and outputs it
+print(12)
